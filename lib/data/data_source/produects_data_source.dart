22a4103892cdb649036f7cf694a5cb22f50d9b3e
kareem_m0hmed <kareemmhammed53@gmail.com>
2026-06-06 20:48:18 +0300

    update: retrofit base url and depend on dio for connection

--- lib/data/data_source/produects_data_source.dart
+++ lib/data/data_source/produects_data_source.dart
@@ -1,12 +1,9 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_retrofit_app/core/network/error/failure.dart';
 import 'package:flutter_retrofit_app/domain/entities/prodeuct_entity.dart';
-import 'package:fpdart/fpdart.dart';
 import 'package:retrofit/retrofit.dart';
-
 part 'produects_data_source.g.dart';
 
-@RestApi(baseUrl: "https://accessories-eshop.runasp.net/api/")
+@RestApi()
 abstract class ProduectsDataSource {
   factory ProduectsDataSource(Dio dio) = _ProduectsDataSource;
 
