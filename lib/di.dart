92b5e38a58ad8101f5a9188770c936d3f8c77eb0
kareem_m0hmed <kareemmhammed53@gmail.com>
2026-06-06 21:40:01 +0300

    feat: add env files and encript base url

--- lib/di.dart
+++ lib/di.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_retrofit_app/data/data_source/produects_data_source.dart';
 import 'package:flutter_retrofit_app/data/repo/products_repo_impl.dart';
 import 'package:flutter_retrofit_app/domain/repo/products_repo.dart';
+import 'package:flutter_retrofit_app/env/env.dart';
 import 'package:flutter_retrofit_app/presentation/cubit/products_cubit.dart';
 import 'package:get_it/get_it.dart';
 
@@ -19,7 +20,7 @@ abstract class InjectionHelper {
     getIt.registerLazySingleton<Dio>(
       () => Dio(
         BaseOptions(
-          baseUrl: 'https://accessories-eshop.runasp.net/api/',
+          baseUrl: Env.baseUrl,
           connectTimeout: const Duration(seconds: 30),
           receiveTimeout: const Duration(seconds: 30),
         ),
